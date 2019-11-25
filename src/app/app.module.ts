d9ee65d77d81e53dca99957318a0c5c464888b49
John Chappelear <john.chappelear@gmail.com>
2019-11-24 19:26:23 -0500

    before cloning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { NewUserComponent } from './new-user/new-user/new-user.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
